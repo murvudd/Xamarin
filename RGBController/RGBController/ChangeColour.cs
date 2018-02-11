bdc4a184ff54c4ae56dfe5c26fdec027ecacdc9d
murvudd <oddajcieminick@gmail.com>
2018-02-11 12:06:40 +0100

    app created

---
+++ RGBController/RGBController/ChangeColour.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Microsoft.AspNet.SignalR.Client;
+
+namespace RGBController
+{
+    [Activity(Label = "ChangeColour")]
+    public class ChangeColour : Activity
+    {
+
+        protected HubConnection connection = new HubConnection("http://kolejkomatapp4.azurewebsites.net/signalr/hubs");
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.ChangeColour);
+            // Create your application here
+            var myHub = connection.CreateHubProxy("mainHub");
+            connection.Start().ContinueWith(task =>
+            {
+                if (task.IsFaulted)
+                {
+                    Console.WriteLine("There was an error opening the connection:{0}",
+                                  task.Exception.GetBaseException());
+                }
+                else
+                {
+                    Console.WriteLine("Connected");
+                }
+            }).Wait();
+            //myHub.Invoke<string>("Hello", "witam z apki").Wait();
+
+            FindViewById<SeekBar>(Resource.Id.seekBarRed).ProgressChanged += (s, e) =>
+            {
+                FindViewById<TextView>(Resource.Id.textViewR).Text = string.Format("R {0}", e.Progress);
+                myHub.Invoke<string>("Hello", "Red " + e.Progress);
+            };
+
+            FindViewById<SeekBar>(Resource.Id.seekBarGreen).ProgressChanged += (s, e) =>
+            {
+                FindViewById<TextView>(Resource.Id.textViewG).Text = string.Format("G {0}", e.Progress);
+                myHub.Invoke<string>("Hello", "Green " + e.Progress);
+            };
+
+            FindViewById<SeekBar>(Resource.Id.seekBarBlue).ProgressChanged += (s, e) =>
+            {
+                FindViewById<TextView>(Resource.Id.textViewB).Text = string.Format("B {0}", e.Progress);
+                myHub.Invoke<string>("Hello", "Blue " + e.Progress);
+            };
+
+            //connection.Stop();
+
+        }
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            connection.Stop();
+        }
+    }
+}
---
+++ RGBController/RGBController/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace RGBController
+{
+    [Activity(Label = "RGBController", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            Button mainbtn1 = FindViewById<Button>(Resource.Id.mainbtn1);
+            mainbtn1.Click += (o, e) => StartActivity(typeof(ChangeColour));
+
+            Button mainbtn2 = FindViewById<Button>(Resource.Id.mainbtn2);
+            mainbtn2.Click += (o, e) => StartActivity(typeof(LogIn));
+
+            Button mainbtn3 = FindViewById<Button>(Resource.Id.mainbtn3);
+            mainbtn3.Click += (o, e) => StartActivity(typeof(About));
+        }
+    }
+}
+
